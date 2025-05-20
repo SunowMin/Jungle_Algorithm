ec1464675cd34bb0262269b0c6ae6465ebc31c3b
Sumin Kim <kko3093@gmail.com>
2025-05-20 20:03:59 +0900

    [Bronze II] Title: 수 정렬하기, Time: 56 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,39 +1,14 @@
-def merge_sort(arr):
-    if len(arr) <= 1:
-        return arr
-
-    mid = len(arr) // 2
-    left = merge_sort(arr[:mid])
-    right = merge_sort(arr[mid:])
-
-    return merge(left, right)
-
-def merge(left, right):
-    result = []
-    i = j = 0
-
-    # 두 리스트를 정렬하면서 병합
-    while i < len(left) and j < len(right):
-        if left[i] < right[j]:
-            result.append(left[i])
-            i += 1
-        else:
-            result.append(right[j])
-            j += 1
-
-    # 남은 요소 추가
-    result.extend(left[i:])
-    result.extend(right[j:])
-
-    return result
-
 # 입력 받기
-n = int(input())
-arr = [int(input()) for _ in range(n)]
+N = int(input())           # 첫 번째 줄: 수의 개수
+numbers = []               # 숫자들을 저장할 리스트
 
-# 정렬
-sorted_arr = merge_sort(arr)
+for _ in range(N):
+    num = int(input())
+    numbers.append(num)    # 리스트에 숫자 추가
 
-# 출력
-for num in sorted_arr:
+# 정렬하기
+numbers.sort()
+
+# 출력하기
+for num in numbers:
     print(num)
