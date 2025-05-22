8d6ea322d1d300a1a32d3b8a57476b4af6355ac7
Sumin Kim <kko3093@gmail.com>
2025-05-22 20:59:33 +0900

    [Bronze I] Title: 더하기 사이클, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.py
@@ -1,32 +1,20 @@
-import sys
-#sys.stdin = open("input.txt","r")
-def make(n):
-     
-    left_lst = list(str(n))
-    
-    left_lst = list(map(int, left_lst))
-    #print(left_lst)
-    if len(left_lst) == 1:
-        left_lst.insert(0,0)
-    
-    
-    # temp=left_lst
-    
-    return left_lst
-def cicle(ref,lst_prev,cnt):
-    
-  
-    n = lst_prev[0] + lst_prev[1]
-    if cnt != 0:
-        compare1 =(lst_prev[0]*10) +(lst_prev[1])
-        if compare1 == ref:
-            return cnt
-    cnt +=1
-    
-    #print(compare1)
-    lst_next =make(n)
-    cnt = cicle(ref,[lst_prev[1],lst_next[1]],cnt)
-    return cnt
-n = int(sys.stdin.readline())
+n = int(input())
+num = n
+cnt = 0
 
-print(cicle(n,make(n),0))
+while True:
+    # 십의 자리
+    a = num // 10 
+    # 일의 자리
+    b = num % 10
+    # 새로운 수의 일의 자리
+    c = (a+b)%10
+    num = (b*10) + c
+
+    cnt += 1
+
+    # 종료 조건
+    if n == num:
+        break
+
+print(cnt)
