1d5c6950e1d2e4919323f41810b8acc98e1475cf
Sumin Kim <kko3093@gmail.com>
2025-05-28 21:03:29 +0900

    [Silver IV] Title: 카드2, Time: 212 ms, Memory: 51848 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,23 +1,19 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
+from collections import deque
 
-n = int(input())
-# deque 클래스의 queue 객체 생성
+# 큐 생성
 queue = deque()
 
-for i in range(1,n+1):
-    # 리스트에 값 추가
+n = int(input())
+
+for i in range(1, n+1):
     queue.append(i)
 
-for i in range(n):
-    # 카드가 한 장 남으면
-    if len(queue) == 1:
-        print(queue.pop())
-    else:
-        # 제일 위에 있는 카드 버리기-popleft
-        queue.popleft()
+while len(queue) > 1:
+    # 제일 위에 있는 카드 버리기
+    queue.popleft()
+    # 그 다음 제일 위에 있는 카드를 제일 아래에 있는 카드 밑으로 옮김
+    queue.append(queue.popleft())
 
-        # 맨 위에 있던 카드 버리기-pop, 그 다음 위에 있는 카드 값을 맨 아래에 추가-appendleft
-        temp = queue.popleft()
-        queue.append(temp)
+print(queue[0])
