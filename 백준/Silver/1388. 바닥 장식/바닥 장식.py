45dd980b7f063fb6b318ba62a652c86c8b668d39
Sumin Kim <kko3093@gmail.com>
2025-06-05 02:53:29 +0900

    [Silver IV] Title: 바닥 장식, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1388. 바닥 장식/바닥 장식.py
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.py
@@ -1,37 +1,41 @@
 import sys
+sys.setrecursionlimit(10*6)
+input=sys.stdin.readline
 
-# 입력 받기
-n, m = map(int, input().split())
-floor = [list(input().strip()) for _ in range(n)]  # 바닥 정보 저장
+n,m = map(int,input().split())
+board=[]
 
-# 방문 여부 체크용
-visited = [[False]*m for _ in range(n)]
+for _ in range(n):
+    a=list(input().strip())
+    board.append(a)
 
-# DFS 함수 정의
-def dfs(x, y, symbol):
-    visited[x][y] = True  # 현재 칸 방문 처리
+visited=[]
 
-    # 연결된 방향으로만 이동
-    if symbol == '-':
-        # 오른쪽으로만 연결
-        ny = y + 1
-        if ny < m and not visited[x][ny] and floor[x][ny] == '-':
-            dfs(x, ny, '-')
+for _ in range(n):
+    a=[False] * m
+    visited.append(a)
 
-    elif symbol == '|':
-        # 아래쪽으로만 연결
-        nx = x + 1
-        if nx < n and not visited[nx][y] and floor[nx][y] == '|':
-            dfs(nx, y, '|')
+def dfs(x,y):
+    visited[x][y] = True
+    current = board[x][y]
 
-# 판자 개수 세기
+    if current == '-':
+        ny = y+1
+        if ny<m and not visited[x][ny] and board[x][ny] == '-':
+            dfs(x, ny)
+
+    if current == '|':
+        nx = x+1
+        if nx<n and not visited[nx][y] and board[nx][y] == '|':
+            dfs(nx, y)
+
+
 count = 0
+
 for i in range(n):
     for j in range(m):
         if not visited[i][j]:
-            # 방문하지 않은 곳에서 새 판자 시작
-            dfs(i, j, floor[i][j])
-            count += 1
-
-# 정답 출력
+            dfs(i,j)
+            count+=1
+            
 print(count)
