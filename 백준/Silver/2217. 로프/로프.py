cd33b9cfe87740e0ffca7e79a4ba94decc2ca8aa
Sumin Kim <kko3093@gmail.com>
2025-06-11 23:17:37 +0900

    [Silver IV] Title: 로프, Time: 788 ms, Memory: 120224 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.py
+++ 백준/Silver/2217. 로프/로프.py
@@ -7,11 +7,13 @@ a = []
 for _ in range(n):
     a.append(int(input()))
 
+# 10 15 20
+a.sort()
+
 result = []
 for _ in range(n):
-    m = min(a)
-    result.append((m * len(a)))
-    a.remove(m)
+    result.append((a[0] * len(a)))
+    a.remove(a[0])
 
 # print(min(a)*n)
 print(max(result))
